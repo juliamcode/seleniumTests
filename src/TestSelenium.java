41882d446bf07eab0321556030318802a9f975c9
juliamcode <majewska.julia@yahoo.com>
2020-09-15 17:20:36 +0200

    tests v1

--- src/TestSelenium.java
+++ src/TestSelenium.java
@@ -1,12 +1,14 @@
+import org.junit.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebElement;
 import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 
 public class TestSelenium {
 
@@ -15,44 +17,53 @@ public class TestSelenium {
     }
 
     public static WebDriver driver = new FirefoxDriver();
+    public static String checkSolution = "solution";
 
 
-    public static void main(String[] args){
+    @Test
+    public void excercise1() {
 
-        // excercise 1
-
+        String excercise1address = "https://antycaptcha.amberteam.pl/exercises/exercise1?seed=6b42fcfa-5b3f-4ebe-8de2-7e260a0f0ef8";
         String btnButton1 = "btnButton1";
         String btnButton2 = "btnButton2";
-        String checkSolution = "solution";
-        String excercise1address = "https://antycaptcha.amberteam.pl/exercises/exercise1?seed=6b42fcfa-5b3f-4ebe-8de2-7e260a0f0ef8";
 
-//        driver.get(excercise1address);
-//
-//        driver.findElement(By.id(btnButton1)).click();
-//        driver.findElement(By.id(btnButton2)).click();
-//        driver.findElement(By.id(btnButton1)).click();
-//        driver.findElement(By.id(checkSolution)).click();
+        driver.get(excercise1address);
 
-        // excercise 2
+        driver.findElement(By.id(btnButton1)).click();
+        driver.findElement(By.id(btnButton2)).click();
+        driver.findElement(By.id(btnButton1)).click();
 
-//        String excercise2address = "https://antycaptcha.amberteam.pl/exercises/exercise2?seed=977417bc-98f9-46e5-a0b8-6ae94be225f7";
-//        driver.get(excercise2address);
-//        String inputID = "t14";
-//        driver.findElement(By.id(inputID)).clear();
-//        driver.findElement(By.id(inputID)).sendKeys("Air environment author.");
-//        driver.findElement(By.id(btnButton1)).click();
-//        driver.findElement(By.id(checkSolution)).click(); //OK. Good answer
+        checkSolution();
+    }
 
-        // excercise 3
+    @Test
+    public void excercise2() {
 
-//        String excercise3address = "https://antycaptcha.amberteam.pl/exercises/exercise3?seed=ea74c46a-6546-4ba5-a996-2384bff439a9";
-//        driver.get(excercise3address);
-//        String dropdownListId = "s13";
-//        Select drp = new Select(driver.findElement(By.id(dropdownListId)));
-//        drp.selectByVisibleText("Freudian Gilt");
-//        driver.findElement(By.id(checkSolution)).click(); //OK. Good answer
+        String excercise2address = "https://antycaptcha.amberteam.pl/exercises/exercise2?seed=977417bc-98f9-46e5-a0b8-6ae94be225f7";
+        String btnButton1 = "btnButton1";
+        driver.get(excercise2address);
+        String inputID = "t14";
+        driver.findElement(By.id(inputID)).clear();
+        driver.findElement(By.id(inputID)).sendKeys("Air environment author.");
+        driver.findElement(By.id(btnButton1)).click();
 
-        // excercise 4
+        checkSolution();
+    }
+
+    @Test
+    public void excercise3() {
+
+        String excercise3address = "https://antycaptcha.amberteam.pl/exercises/exercise3?seed=ea74c46a-6546-4ba5-a996-2384bff439a9";
+        driver.get(excercise3address);
+        String dropdownListId = "s13";
+        Select drp = new Select(driver.findElement(By.id(dropdownListId)));
+        drp.selectByVisibleText("Freudian Gilt");
+
+        checkSolution();
+    }
+
+    @Test
+    public void excercise4() {
 
         String excercise4address = "https://antycaptcha.amberteam.pl/exercises/exercise4?seed=ea74c46a-6546-4ba5-a996-2384bff439a9";
         driver.get(excercise4address);
@@ -62,17 +73,17 @@ public class TestSelenium {
         clickElementInRadioButtonsList("s2", "v22");
         clickElementInRadioButtonsList("s3", "v03");
 
-        driver.findElement(By.id(checkSolution)).click(); //OK. Good answer
+        checkSolution();
+    }
 
-        // excercise 5
+    @Test
+    public void excercise5() {
         // 404 page not found
+    }
 
-
-
-
-
-
-
+    public static void checkSolution() {
+        driver.findElement(By.id(checkSolution)).click();
+        assertEquals(driver.findElement(By.id("trail")).getText(), "OK. Good answer");
     }
 
     public static void clickElementInRadioButtonsList(String elementsName, String elementsValue) {
